0b9dc0c27dc5428beb0501036db034b606820122
tsuyoshi <nakasone@std.it-college.ac.jp>
2016-02-17 12:23:14 +0900

    Initial commit. modのエントリポイント作成

---
+++ src/main/java/jp/typosone/minecraft/wall_block_light/ClientProxy.java
@@ -0,0 +1,7 @@
+package jp.typosone.minecraft.wall_block_light;
+
+/**
+ * Created by tsuyoshi on 2016/02/17.
+ */
+public class ClientProxy extends CommonProxy {
+}
---
+++ src/main/java/jp/typosone/minecraft/wall_block_light/WallBlockLightCore.java
@@ -0,0 +1,40 @@
+package jp.typosone.minecraft.wall_block_light;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraft.block.Block;
+
+/**
+ * This class is mod's main class
+ */
+@Mod(
+        modid = "wall_block_light"
+        , name = "Wall Block Light"
+        , version = "0.1"
+)
+public class WallBlockLightCore {
+    @Mod.Instance("wall_block_light")
+    public static WallBlockLightCore instance;
+
+    @SidedProxy(
+            clientSide = "jp.typosone.minecraft.wall_block_light.ClientProxy"
+            , serverSide = "jp.typosone.minecraft.wall_block_light.CommonProxy"
+    )
+    public static CommonProxy proxy;
+
+
+    public static Block wollWallBlockGenerator;
+    public static Block woolWallBlock;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+
+    }
+}
